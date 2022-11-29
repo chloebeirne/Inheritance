13f57cc5569b3dacb9176d14723b791be5062a12
g00399850 <g00399850@atu.ie>
2022-11-29 14:48:45 +0000

    Commit finished code

--- src/main/java/ie/atu/week10/Customer.java
+++ src/main/java/ie/atu/week10/Customer.java
@@ -4,6 +4,9 @@ public class Customer extends Person{
     private String customerNumber;
     private boolean mailingList;
 
+    public Customer() {
+    }
+
     public Customer(String name, String address, String number, String customerNumber, boolean mailingList)
     {
         super(name, address, number);
--- src/main/java/ie/atu/week10/PersonApp.java
+++ src/main/java/ie/atu/week10/PersonApp.java
@@ -7,5 +7,15 @@ public class PersonApp {
         System.out.println(chloe.toString());
         Customer brian = new Customer("Peter", "Mervue", "0877654321", "9999", true);
         System.out.println(brian.toString());
+
+        Customer patrick = new Customer();
+        patrick.setName("Patrick");
+        patrick.setAddress("Far away");
+        patrick.setNumber("0831243567");
+        patrick.setCustomerNumber("2134");
+
+        System.out.println("Name: " + patrick.getName() + "Address: " + patrick.getAddress() + "Number: " + patrick.getNumber() + "Customer Number: " + patrick.getCustomerNumber());
     }
+
+
 }
